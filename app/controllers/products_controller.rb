dc43e585b440aa2d9f31c1ba3e9ec275d1d107e3
Olivia <oereeve@gmail.com>
2017-04-25 15:24:20 -0400

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,53 @@
+class ProductsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+  def index
+    @products = Product.order("#{sort_column} #{sort_direction}")
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @products = Product.all
+    @product = Product.create(product_params)
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @products = Product.all
+    @product = Product.find(params[:id])
+
+    @product.update_attributes(product_params)
+  end
+
+  def delete
+    @product = Product.find(params[:product_id])
+  end
+
+  def destroy
+    @products = Product.all
+    @product = Product.find(params[:id])
+    @product.destroy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+
+  def sort_column
+    Product.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+end
